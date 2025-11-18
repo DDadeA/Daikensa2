888a0a241ab530e80edc10d3267b8518c2e4f691
Aseli-No4488 <aseli4488@gmail.com>
2025-11-19 00:14:21 +0900

    Add: gemini 3 preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -963,7 +963,7 @@
 						bind:value={GEMINI_MODEL}
 						onchange={() => localStorage.setItem('GEMINI_MODEL', GEMINI_MODEL)}
 					>
-						{#each ['gemini-2.5-pro', 'gemini-2.5-flash', 'gemini-2.5-flash-lite'] as model}
+						{#each ['gemini-3-pro-preview', 'gemini-2.5-pro', 'gemini-2.5-flash', 'gemini-2.5-flash-lite'] as model}
 							<option value={model}>{model}</option>
 						{/each}
 					</select>
